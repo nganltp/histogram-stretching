b30c2e288cc8998d20afafcd1f388ee3c052aed1
nganltp <16520792@gm.uit.edu.vn>
2018-07-26 16:25:50 +0700

    done

--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,7 @@ using namespace std;
 void drawHis(Mat src, Mat &histImage)
 {
 	vector<Mat> bgr_planes;
-	int hist_w = 1024; int hist_h = 300, histSize = 512;
+	int hist_w = 512; int hist_h = 300, histSize = 512;
 	int bin_w = cvRound((double)hist_w / histSize);
 	split(src, bgr_planes);
 	float range[] = { 0, 256 };
@@ -48,85 +48,104 @@ bool histogramStretch(Mat Msrc, unsigned char * src, unsigned char *dst, int col
 	for (int x = 0; x < cols; x++)
 		for (int y = 0; y < rows; y++)
 		{
-			hist[(int)(Msrc.at<uchar>(y,x))] ++;
+			hist[(int)(Msrc.at<uchar>(y, x))] ++;
 		}
 	for (int i = 0; i < 256; i++)
-		cout << i<<": "<<hist[i]<<endl;
+		cout << i << ": " << hist[i] << endl;
+
+	/*Init*/
 	int i, j;
-	int T = 10; // giá trị ngưỡng dùng xác định hai biên
+	int T = 10; // thresh
 	int a = 0, b = 0;
 	int A = 0, B = 255;
-	unsigned char cdfLut[256]; // bảng tra
+	unsigned char cdfLut[256]; // lookup
 	int sum = 0;
 
-	// xác định giá trị a, b
+	// detect value for a,b
 	for (i = 0; i<256; i++)
 		if (hist[i] > T)
 		{
-			if(a==0) a = i;
+			if (a == 0) a = i;
 			for (j = 255; j>i; j--)
 				if (hist[j]>T)
 				{
 					b = j;
-					j = 0; i = 300; // thoat
+					j = 0; i = 300; // out
 				}
 		}
-	cout << "a: " << a<<endl;
+	cout << "a: " << a << endl;
 	cout << "b: " << b << endl;
-	// tính giá trị cho bảng tra
+	// calculator for lookup table
 	if ((a>A && b <= B) || (a >= A && b<B))
 	{
 		for (i = 0; i<256; i++)
 		{
 			//if((b-a)!=0)
-			cdfLut[i] = (unsigned char) (int)((/*hist[i]*/ i - a)*(B - A) / (b - a) + A);
+			cdfLut[i] = (unsigned char)(int)((/*hist[i]*/ i - a)*(B - A) / (b - a) + A);
 			//else cdfLut[i] = 0;
 		}
 		cout << endl << "------------------------" << endl << "Table" << endl;
 		for (int tab = 0; tab < 256; tab++)
-			cout << tab << ": " << cdfLut[tab]<<endl;
-		//can bang histogram
+			cout << tab << ": " << cdfLut[tab] << endl;
+		//update new value
 		return table_lookup(src, dst, cdfLut, cols, rows);
 	}
-	return false; // các giá trị không được xác định
+	return false; // not exist value
 }
 
 Mat convertToMat(unsigned char *buffer, int width, int height) {
 	Mat tmp(height, width, CV_8UC1);
 	for (int x = 0; x < height; x++)
 		for (int y = 0; y < width; y++) {
-		{
-			int value = (int)buffer[x * width + y];
-			tmp.at<uchar>(x, y) = value;
+			{
+				int value = (int)buffer[x * width + y];
+				tmp.at<uchar>(x, y) = value;
+			}
 		}
-	}
 	return tmp;
 }
-
-//myMat = convertToMat(buffer);
 
 int main()
 {
 	Mat src, dst;
-	src = imread("1.png",0);
-	//imshow("srcb", src);
-	//cvtColor(src, dst, COLOR_BGR2GRAY);
+
+	/*Init histogram*/
+	int hist_w = 512; int hist_h = 300, histSize = 512;
+	int bin_w = cvRound((double)hist_w / histSize);
+	Mat histSrc(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+	Mat histDst(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+
+	string path;
+	cin >> path;
+	src = imread(path);
+	//src = imread("1.png");
+	cvtColor(src, src, COLOR_BGR2GRAY);
+
+	drawHis(src, histSrc);
+
 	unsigned char *csrc = src.data;
-	//for (unsigned int i = 0; i< src.cols*src.rows; i++)
-		//cout << csrc[i] << endl;
-	unsigned char *cdst = new unsigned char[src.cols*src.rows];	
-	histogramStretch(src,csrc, cdst, src.cols, src.rows);
-//	for (unsigned int i = 0; i< src.cols*src.rows; i++)
+	unsigned char *cdst = new unsigned char[src.cols*src.rows];
+
+	/*STRETCHING*/
+	histogramStretch(src, csrc, cdst, src.cols, src.rows);
+	//	for (unsigned int i = 0; i< src.cols*src.rows; i++)
 	//	cout << cdst[i]<<endl;
+
+	/* convert unsigned char to Mat */
 	dst = convertToMat(cdst, src.cols, src.rows);
-	//src = con
+	drawHis(dst, histDst);
+
+	imwrite("Output.png", dst);//save Output Image
+
 	namedWindow("src", WINDOW_AUTOSIZE);
-	namedWindow("dst", WINDOW_AUTOSIZE);
-	
-	imshow("src", src);
-	imshow("dst", dst);
-	//int a;
-	//cin >> a;
+	//	namedWindow("dst", WINDOW_AUTOSIZE);
+	//	namedWindow("HistSrc");
+	//	namedWindow("HistDst");
+
+	imshow("Src", src);
+	imshow("Dst", dst);
+	imshow("HistSrc", histSrc);
+	imshow("HistDst", histDst);
 	waitKey(0);
 	return 0;
 }
